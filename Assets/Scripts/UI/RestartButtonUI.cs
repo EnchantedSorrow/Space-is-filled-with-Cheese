30b9059867b21d142b823a73e3299b7cefe29b0e
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-08-30 13:23:14 +1200

    Created script to manage restart button

---
+++ Assets/Scripts/UI/RestartButtonUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class RestartButtonUI : MonoBehaviour
+{
+    [field: SerializeField] public UIDocument UIDocument { get; private set; }
+    
+    //UI elements
+    private Button _restartButton;
+    
+    void OnValidate()
+    {
+        if (UIDocument == null) Debug.LogWarning("GameUI UIDocument is null");
+    }
+    
+    void Awake()
+    {
+        UIDocument = GetComponent<UIDocument>();
+        _restartButton = UIDocument.rootVisualElement.Q<Button>("RestartButton");
+    }
+    
+    void Start()
+    {
+        //Hide Restart Button
+        HideRestartButton();
+    }
+
+    private void HideRestartButton()
+    {
+        if (_restartButton != null) _restartButton.style.display = DisplayStyle.None;
+    }
+
+    void ShowRestartButton()
+    {
+        if (_restartButton != null) _restartButton.style.display = DisplayStyle.Flex;
+    }
+}
--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -33,6 +32,6 @@ public class ScoreUI : MonoBehaviour
 
     void UpdateScoreUI(int score)
     {
-        _scoreText.text = $"Score: {score}";
+        if (_scoreText != null) _scoreText.text = $"Score: {score}";
     }
 }
